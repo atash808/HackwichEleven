95cf28e8804439d61745457dd4c4bbd579628fed
atash808 <atash@hawaii.edu>
2022-11-16 19:33:51 -1000

    Completed Parts 1-7

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -2,18 +2,44 @@
 //  ViewController.swift
 //  HackwichEleven
 //
-//  Created by Flynn Porter on 11/16/22.
+//  Created by Atash Imanverdi on 11/16/22.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
+    
+    
+    let regionRadius = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+       centerMapOnLocation(location: initialLocation)
         // Do any additional setup after loading the view.
+        
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        
+        mapView.addAnnotation(restaurantOne)
     }
 
+    func centerMapOnLocation(location:CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: CLLocationDistance(regionRadius), longitudinalMeters: CLLocationDistance(regionRadius))
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        
+    }
 
+    
+    
+    
+    
 }
 
